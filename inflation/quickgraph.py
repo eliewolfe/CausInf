e6d4dc5ab1598d7817561786e91cb3f9b6e607a6
Elie Wolfe <elupus@gmail.com>
2021-01-26 15:52:35 -0500

    Switched to native igraph function for exploring paths

--- inflation/quickgraph.py
+++ inflation/quickgraph.py
@@ -7,28 +7,6 @@ Learning a little bit about the inflation graph from the original graph
 import numpy as np
 from igraph import Graph
 
-# from igraph import * #Not needed, since we are using methods built into the object...!
-
-def adjlist_find_paths(graph, n, m , path=[]):
-  "Find paths from node index n to m using adjacency list a."
-  path = path + [n]
-  if n == m:
-    return [path]
-  paths = []
-  for child in graph.successors(n):
-    if child not in path:
-      child_paths = adjlist_find_paths(graph, child, m, path)
-      for child_path in child_paths:
-        paths.append(child_path)
-  return paths
-
-def paths_from_to(graph, source, dest):
-  "Find paths in graph from vertex source to vertex dest."
-  n = source.index
-  m = dest.index
-  return adjlist_find_paths(graph, n, m)
-
-
 def ToTopologicalOrdering(g):
     return g.permute_vertices(np.argsort(g.topological_sorting('out')).tolist())
 
@@ -62,26 +40,16 @@ def LearnParametersFromGraph(origgraph, hasty=False):
         
         def FindBestScreeningOffSet(root, observed, g):
             screeningset = np.intersect1d(root["children"], observed["ancestors"]).tolist()
-            validvars=[]
+
             for sidx in screeningset:
-                        
-                p=paths_from_to(g, g.vs[sidx], observed)
-                
-                        
-                for pat in p:
-                            
-                    pat.remove(sidx)
-                    check=False
-                            
-                    for v in screeningset:
-                        check=check | any(np.array(pat) == v)
-                                
-                if check == False:
-                                
-                    validvars.append(sidx)
-          
-            validvars.append(observed.index)
-            return validvars
+
+                screeningset_rest = set(screeningset)
+                screeningset_rest.remove(sidx)
+                if not all(screeningset_rest.isdisjoint(directed_path) for directed_path in g.get_all_simple_paths(sidx, to=observed)):
+                    screeningset.remove(sidx)
+
+            screeningset.append(observed.index)
+            return screeningset
 
         determinism_checks = [(root, FindScreeningOffSet(verts[root], v)) for v in g.vs[has_grandparents] for root in
                               np.setdiff1d(v["roots_of"], v["parents"])]
@@ -103,33 +71,12 @@ def LearnSomeInflationGraphParameters(g, inflation_order):
     num_vars = inflationcopies.sum()
     return obs_count, num_vars, names[latent_count:]
 
-def FindBestScreeningOffSet(root, observed, g):
-    screeningset = np.intersect1d(root["children"], observed["ancestors"]).tolist()
-    validvars=[]
-    for sidx in screeningset:
-                        
-        p=paths_from_to(g, g.vs[sidx], observed)
-                
-                        
-        for pat in p:
-                            
-            pat.remove(sidx)
-            check=False
-                            
-            for v in screeningset:
-                check=check | any(np.array(pat) == v)
-                                
-        if check == False:
-                                
-            validvars.append(sidx)
-          
-    validvars.append(observed.index)
-    return validvars
+
 
 if __name__ == '__main__':
 
-    TGraph=Graph.Formula("U1->A:C,U2->A:B:D,U3->B:C:D,A->B,C->D")
-    #TGraph=Graph.Formula("U1->A:B:C:D,D->C,C->X,A->X,B->X,D->X")
+    #TGraph=Graph.Formula("U1->A:C,U2->A:B:D,U3->B:C:D,A->B,C->D")
+    TGraph=Graph.Formula("U1->A:B:C:D,D->C,C->X,A->X,B->X")
     #TGraph=Graph.Formula("U1->X->A->B,U2->A:B")
 
     g = ToTopologicalOrdering(TGraph)
@@ -149,24 +96,13 @@ if __name__ == '__main__':
     verts["roots_of"] = [np.intersect1d(anc, root_vertices).tolist() for anc in g.vs["ancestors"]]
 
 
-    filtered_determinism_checks = [(root, FindBestScreeningOffSet(verts[root], v,g)) for v in g.vs[has_grandparents] for root in np.setdiff1d(v["roots_of"], v["parents"])]
+    #filtered_determinism_checks = [(root, FindBestScreeningOffSet(verts[root], v,g)) for v in g.vs[has_grandparents] for root in np.setdiff1d(v["roots_of"], v["parents"])]
 
-    print(filtered_determinism_checks)
-
-    """
-    p=paths_from_to(g, verts[3], verts[5])
-    print(p)
-    """
-    """
-    names,parents,roots_of,determinism_checks, filtered_determinism_checks=LearnParametersFromGraph(TGraph, hasty=False)
-    
-    
+    names, parents_dict, roots_dict, determinism_checks, filtered_determinism_checks = LearnParametersFromGraph(TGraph)
     print(names)
-    print(parents)
-    print(roots_of)
     print(determinism_checks)
     print(filtered_determinism_checks)
-    """
+
     inflation_order=2
     ExSets=[]
     for screenoffs in filtered_determinism_checks:
@@ -176,6 +112,22 @@ if __name__ == '__main__':
         U1=screenoffs[0]
         roots=verts["roots_of"][Y]
         notroots=list(np.setdiff1d(roots,root_vertices))
+
+        """
+        Elie's version would look like this:
+        U2 = set([])
+        U2.update(verts["roots_of"][X] for X in Xs)
+        U2.remove(U1)
+        
+        Z = set(nonroot_vertices)
+        Z.remove(Y)
+        Z.difference_update(X)
+        for u2 in U2:
+            Z.difference_update(verts["descendants"][u2])
+        
+        
+        
+        """
 
         for X in Xs:
 
