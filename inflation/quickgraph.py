edd663dbb41defffc885f15d34191e9f25170e4e
Elie Wolfe <elupus@gmail.com>
2021-01-26 17:51:52 -0500

    Adding Expressible Set precursors to graph learning
    Started writing FindExpressibleSets as a function within LearnParametersFromGraph.
    
    NOTE: Needs overhaul to accommodate tuple-valued latent variables being screened off.

--- inflation/quickgraph.py
+++ inflation/quickgraph.py
@@ -22,11 +22,11 @@ def LearnParametersFromGraph(origgraph, hasty=False):
     # verts["outdegree"]=g.outdegree() #Not needed
     verts["grandparents"] = g.neighborhood(None, order=2, mode='in', mindist=2)
     # verts["parents_inclusive"]=g.neighborhood(None, order=1, mode='in', mindist=0) #Not needed
-    has_grandparents = [idx for idx, v in enumerate(g.vs["grandparents"]) if len(v) >= 1]
-    verts["isroot"] = [0 == i for i in g.vs["indegree"]]
+    has_grandparents = [idx for idx, v in enumerate(verts["grandparents"]) if len(v) >= 1]
+    verts["isroot"] = [0 == i for i in verts["indegree"]]
     root_vertices = verts.select(isroot=True).indices
     nonroot_vertices = verts.select(isroot=False).indices
-    verts["roots_of"] = [np.intersect1d(anc, root_vertices).tolist() for anc in g.vs["ancestors"]]
+    verts["roots_of"] = [np.intersect1d(anc, root_vertices).tolist() for anc in verts["ancestors"]]
     #print(verts["parents"])
     #de= g.successors(verts)
     #print(de)
@@ -55,8 +55,38 @@ def LearnParametersFromGraph(origgraph, hasty=False):
                               np.setdiff1d(v["roots_of"], v["parents"])]
         
         filtered_determinism_checks = [(root, FindBestScreeningOffSet(verts[root], v,g)) for v in g.vs[has_grandparents] for root in np.setdiff1d(v["roots_of"], v["parents"])]
+
+        #We are going to want to adjust this code to handle screening off of multiple roots at once.
+        #Should we be concerned about multiple root screening in terms of determinism checks? YES!!!
+        def FindExpressibleSet(determinism_check, dict_of_roots, dict_of_descendants):
+            Y = determinism_check[1][-1]
+            Xs = determinism_check[1][:-1]
+            U1 = determinism_check[0]
+            roots = dict_of_roots[Y]
+
+            U2 = set([])
+            for X in Xs:
+                U2.update(dict_of_roots[X])
+            U2.remove(U1)
+
+            Zs = set(nonroot_vertices)
+            Zs.remove(Y)
+            Zs.difference_update(Xs)
+            for u2 in U2:
+                Zs.difference_update(dict_of_descendants[u2])
+
+            U3YZ = set([])
+            for Z in Zs:
+                U3YZ.update(dict_of_roots[Z])
+            U3YZ.intersection_update(dict_of_roots[Y])
+            U3YZ.remove(U1)
+            U3YZ.difference_update(U2)
+
+            return tuple([list(U3YZ),(Y,list(Zs),Xs)])
+
+        expressible_sets = [FindExpressibleSet(determinism_check, verts["roots_of"], verts["descendants"]) for determinism_check in filtered_determinism_checks]
         
-    return verts["name"], verts["parents"], verts["roots_of"], determinism_checks, filtered_determinism_checks
+    return verts["name"], verts["parents"], verts["roots_of"], determinism_checks, filtered_determinism_checks, expressible_sets
 
 
 def LearnSomeInflationGraphParameters(g, inflation_order):
@@ -75,8 +105,8 @@ def LearnSomeInflationGraphParameters(g, inflation_order):
 
 if __name__ == '__main__':
 
-    #TGraph=Graph.Formula("U1->A:C,U2->A:B:D,U3->B:C:D,A->B,C->D")
-    TGraph=Graph.Formula("U1->A:B:C:D,D->C,C->X,A->X,B->X")
+    TGraph=Graph.Formula("U1->A:C,U2->A:B:D,U3->B:C:D,A->B,C->D")
+    #TGraph=Graph.Formula("U1->A:B:C:D,D->C,C->X,A->X,B->X")
     #TGraph=Graph.Formula("U1->X->A->B,U2->A:B")
 
     g = ToTopologicalOrdering(TGraph)
@@ -89,19 +119,20 @@ if __name__ == '__main__':
     # verts["outdegree"]=g.outdegree() #Not needed
     verts["grandparents"] = g.neighborhood(None, order=2, mode='in', mindist=2)
     # verts["parents_inclusive"]=g.neighborhood(None, order=1, mode='in', mindist=0) #Not needed
-    has_grandparents = [idx for idx, v in enumerate(g.vs["grandparents"]) if len(v) >= 1]
+    has_grandparents = [idx for idx, v in enumerate(verts["grandparents"]) if len(v) >= 1]
     verts["isroot"] = [0 == i for i in g.vs["indegree"]]
     root_vertices = verts.select(isroot=True).indices
     nonroot_vertices = verts.select(isroot=False).indices
-    verts["roots_of"] = [np.intersect1d(anc, root_vertices).tolist() for anc in g.vs["ancestors"]]
+    verts["roots_of"] = [np.intersect1d(anc, root_vertices).tolist() for anc in verts["ancestors"]]
 
 
     #filtered_determinism_checks = [(root, FindBestScreeningOffSet(verts[root], v,g)) for v in g.vs[has_grandparents] for root in np.setdiff1d(v["roots_of"], v["parents"])]
 
-    names, parents_dict, roots_dict, determinism_checks, filtered_determinism_checks = LearnParametersFromGraph(TGraph)
+    names, parents_dict, roots_dict, determinism_checks, filtered_determinism_checks, expressible_sets = LearnParametersFromGraph(TGraph)
     print(names)
     print(determinism_checks)
     print(filtered_determinism_checks)
+    print(expressible_sets)
 
     inflation_order=2
     ExSets=[]
@@ -112,22 +143,6 @@ if __name__ == '__main__':
         U1=screenoffs[0]
         roots=verts["roots_of"][Y]
         notroots=list(np.setdiff1d(roots,root_vertices))
-
-        """
-        Elie's version would look like this:
-        U2 = set([])
-        U2.update(verts["roots_of"][X] for X in Xs)
-        U2.remove(U1)
-        
-        Z = set(nonroot_vertices)
-        Z.remove(Y)
-        Z.difference_update(X)
-        for u2 in U2:
-            Z.difference_update(verts["descendants"][u2])
-        
-        
-        
-        """
 
         for X in Xs:
 
